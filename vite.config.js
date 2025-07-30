3688dccd3f44242aa7fa9cade7e9790af9c910b2
MaxMeyberg <mmeyberg@stanford.edu>
2025-07-30 14:28:22 -0700

    finished step 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/distributive%20property/', // Use the exact repo name
   plugins: [react()],
 })
